1eede8478d526f806a149f627bf50497e81be8b1
Raul Ibarra <raulibarra.a@gmail.com>
2024-09-29 22:06:51 -0300

    Adding hyperlink support for hidden divs and dynamic content
    Adding queryString handler that will scroll to target div id even if it's not visible at first.
    Also, adding new logic to toggle elements (so we don't have to hardcode the ones to hide)

--- js/anims.js
+++ js/anims.js
@@ -15,3 +15,34 @@ function showGames(){
 function showPersonal(){
     return showDiv("projects-portfolio")
 }
+
+function toggleDiv(divId){
+    var element = document.getElementById(divId);
+    
+    if (!element.hasAttribute("toggle-group"))
+    {   // we can't toggle this item
+        return false;
+    }
+
+    // get toggle group id
+    const groupId = element.getAttribute("toggle-group");
+
+    // hide all elements that have this toggle group, by removing the "visible" class
+    const elements = document.querySelectorAll(`[toggle-group="${groupId}"]`);
+    if (elements == null || elements.length === 0)
+    {
+        // there are no elements to hide
+        return false;
+    }
+
+    // iterate over each element and set their state
+    elements.forEach(function(elementItem)
+    {
+        elementItem.classList.remove('visible');
+        
+        if (elementItem == element)
+        {   // make sure the item to show stays visible
+            elementItem.classList.add('visible');
+        }
+    });
+}
