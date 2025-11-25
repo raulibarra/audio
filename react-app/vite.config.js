94fbe25293c2102a5d541926fb424a051d3f69cb
Raul Ibarra Aranda <raulibarra.a@gmail.com>
2025-11-25 11:35:22 -0300

    updating project deploy configuration

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Use relative paths for assets
   build: {
-    outDir: '../',
-    emptyOutDir: false, // Don't empty the parent directory
+    outDir: '../dist',
+    emptyOutDir: true, // Safe to empty the dist directory
   },
 })
